28629f2babdfa51c79395ce6296b1b6ea7aebcb3
Luigi Montanez <luigi.montanez@gmail.com>
2010-10-20 18:59:23 -0400

    Implement websockets

---
+++ cramp/socket_server.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+require 'cramp'
+require 'usher'
+require 'mongoid'
+require 'json'
+
+Cramp::Websocket.backend = :thin
+Mongoid.configure do |config|
+  file_name = File.join(File.dirname(__FILE__), "..", "config", "mongoid.yml")
+  settings = YAML.load(ERB.new(File.new(file_name).read).result)
+  config.from_hash(settings['development'])
+end
+Dir[File.expand_path("../../app/models/*.rb", __FILE__)].each  { |f| require f }
+
+
+module SocketServer
+
+  class BackfillSocket < Cramp::Websocket
+
+    on_data :process_backfill
+
+    def process_backfill(request_string)
+      request = JSON.parse(request_string)
+      render Activity.any_in(:publisher_ids => request["following_ids"]).
+                      desc(:created_at).
+                      limit(30).
+                      to_json
+    end
+    
+  end
+
+  class LiveSocket < Cramp::Websocket
+    @@users = Set.new
+
+    on_start :user_connected
+    on_finish :user_left
+    periodic_timer :new_activities, :every => 5
+    
+    def user_connected
+      @@users << self
+    end
+
+    def user_left
+      @@users.delete self
+    end
+
+    def new_activities
+      #@@users.each {|u| u.render data }
+    end
+  end
+end
+
+
+routes = Usher::Interface.for(:rack) do
+  add('/backfill').to(SocketServer::BackfillSocket)
+  add('/live').to(SocketServer::LiveSocket)
+end
+
+Rack::Handler::Thin.run routes, :Port => 8080
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  $("#activityTemplate").template("activity");
+
   if (!signed_in && _(store.get("following")).isEmpty()) {
 
     var following = [];
@@ -16,16 +18,6 @@ $(document).ready(function() {
     loadFollowing();
   }
 
-  ws = new WebSocket("ws://localhost:8080");
-  ws.onmessage = function(activity) {
-    $("#stream").prepend("<p class='activity'>" + activity.data + "</p>");
-  };
-  ws.onclose = function() {
-    //debug("Socket closed!");
-  };
-  ws.onopen = function() {
-    //debug("Socket connected!");
-  };
 });
 
 var loadFollowing = function() {
@@ -36,4 +28,26 @@ var loadFollowing = function() {
     following_ul.append('<li><a href="#">' + publisher["name"] + '</a><a class="delete" href="#">Delete</a><div class="clear"></div></li>');
   });
 
+  backfillStream();
+}
+
+var backfillStream = function() {
+  var backfillSocket = new WebSocket("ws://localhost:8080/backfill");
+  backfillSocket.onopen = function() {
+    var following_ids = _(store.get("following")).map(function(obj) {
+      return obj['id'];
+    });
+    requestObject = {'since':'0', 'following_ids':following_ids};
+    backfillSocket.send(JSON.stringify(requestObject));
+  };
+
+  backfillSocket.onmessage = function(payload) {
+    addToStream(JSON.parse(payload.data).reverse());
+  };
+}
+
+var addToStream = function(activities) {
+  _(activities).each(function(activity) {
+    activityQueue.push(activity);
+  });
 }
