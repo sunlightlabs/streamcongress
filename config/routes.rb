844c37ca967a1c1f5c370e504be7de4ab906b3ec
Luigi Montanez <luigi.montanez@gmail.com>
2010-12-07 12:26:31 -0500

    Detect user agent -- support Chrome only

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Streamcongress::Application.routes.draw do
 
   root :to => 'main#index'
   match 'settings' => 'main#settings', :as => :settings
+  match 'chrome' => 'main#chrome', :as => :chrome
+
   match 'auth' => 'sessions#new', :as => :auth
   match 'out' => 'sessions#destroy', :as => :out
 
