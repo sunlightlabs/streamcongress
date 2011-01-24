a7bb7b9e0ee7e8a71b2d4c1b63b2781fce71a07b
Luigi Montanez <luigi.montanez@gmail.com>
2011-01-24 11:25:15 -0500

    Add about and contact page.
    [#8990669]

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Streamcongress::Application.routes.draw do
   root :to => 'main#index'
   match 'settings' => 'main#settings', :as => :settings
   match 'chrome' => 'main#chrome', :as => :chrome
+  match 'about' => 'main#about', :as => :about
+  match 'contact' => 'main#contact', :as => :contact
 
   match 'auth' => 'sessions#new', :as => :auth
   match 'out' => 'sessions#destroy', :as => :out
